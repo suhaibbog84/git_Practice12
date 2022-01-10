e91ba4d10a130dc62303f28cdfe9eeabe4388c76
Suhaib B <suhaib.bog84@yahoo.com>
2022-01-10 14:45:19 -0500

    Work 3 is done and about to be pushed to Github

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
 
-    //Hello all, I hope all is great?
+    //Hello all, (I hope all is great)?
 
     //we are doing great just practicing Git
+
+    // Doing awesome work 3!
+    // Awesome work 3 is done and ready to be committed!
+    // we are ready to work
 
 }
